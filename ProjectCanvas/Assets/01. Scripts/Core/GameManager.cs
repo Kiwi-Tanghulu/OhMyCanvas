19ace03d36e9e470250f4e2673b36be83622f8d8
SEH00N <06psh0501@gmail.com>
2023-12-31 15:57:33 +0900

    create host & client

--- ProjectCanvas/Assets/01. Scripts/Core/GameManager.cs
+++ ProjectCanvas/Assets/01. Scripts/Core/GameManager.cs
@@ -1,6 +1,43 @@
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class GameManager : MonoBehaviour
 {
-	
+	private static GameManager instance = null;
+    public static GameManager Instance {
+        get {
+            if(instance == null)
+                instance = FindObjectOfType<GameManager>();
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private async void Start()
+    {
+        bool authenticated = await SetNetwork();
+        if(authenticated)
+            Debug.Log("Change Scene");
+    }
+
+    private async Task<bool> SetNetwork()
+    {
+        ClientManager.Instance = new ClientManager();
+        bool authenticated = await ClientManager.Instance.InitAsync();
+
+        HostManager.Instance = new HostManager();
+
+        return authenticated;
+    }
 }
